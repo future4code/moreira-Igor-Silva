a446d8115fb51a1aacfc0ecc9cf13c736be6e92a
igorpersah <94700879+igorpersah@users.noreply.github.com>
2021-11-29 17:00:44 -0300

    Exercícios de variáveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,11 @@
+// Exercício de Interpretação de Códigos
+
+// Primeira pergunta - R: 10 5
+// Segunda Pergunta - R: 10 10
+// Terceira Pergunta - R: lep perguntaHoras Lep tempoValor
+
+
+const nome = prompt("Qual a sua idade?")
+const idade = prompt("Qual a sua idade?")
+typeof
+
